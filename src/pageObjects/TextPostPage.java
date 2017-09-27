b1cabd3f6d1776c71c58856c8514bb8bd2244a39
unknown <zahraabdo1@hotmail.com>
2017-09-26 19:23:41 -0500

    Initial commit

---
+++ src/pageObjects/TextPostPage.java
@@ -0,0 +1,53 @@
+package pageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TextPostPage {
+	
+	private static WebElement waitToClick(WebDriver driver, By locator) {
+		return new WebDriverWait(driver, 10)
+				.until(ExpectedConditions.elementToBeClickable(locator));
+	}
+	
+	public static WebElement newTextPostButton(WebDriver driver) {
+		return waitToClick(driver, By.cssSelector("i.icon_post_text"));
+				
+	}
+
+	public static WebElement titleField(WebDriver driver) {
+		return waitToClick(driver, By.cssSelector("div.title-field .editor.editor-plaintext"));
+		
+	}
+	
+	public static WebElement mainTextField(WebDriver driver) {
+		return waitToClick(driver, By.cssSelector("div.editor.editor-richtext"));
+		
+	}
+	
+	public static WebElement tagField(WebDriver driver) {
+		return waitToClick(driver, By.cssSelector("div.tag-input-wrapper .editor.editor-plaintext"));
+		
+		
+	}
+	
+	public static WebElement postButton(WebDriver driver) {
+		return waitToClick(driver, By.cssSelector("button.button-area.create_post_button"));
+	}
+	
+	public static WebElement createdTextPost(WebDriver driver, String random) {
+		return waitToClick(driver, By.xpath("//*[contains(text(), '" + random +"')]"));
+		
+	}
+	
+	public static List<WebElement> disappearPost(WebDriver driver) {
+		return new WebDriverWait(driver, 10)
+				.until(ExpectedConditions.numberOfElementsToBeLessThan(By.cssSelector("div.post-container-inner"), 1));
+	}
+	
+}
---
+++ src/testObjects/TextPostTest.java
@@ -0,0 +1,59 @@
+package testObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import flowObjects.LogInFlow;
+import flowObjects.TextPostFlow;
+import pageObjects.TextPostPage;
+
+
+public class TextPostTest {
+
+	WebDriver driver;
+
+	
+	@BeforeTest
+	@Parameters({"browser", "username", "password"})
+	public void setUp(String browser, String username, String password) throws Exception {
+		if (browser.equalsIgnoreCase("Chrome")) {
+			//Create a new instance of the Chrome driver
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Zee\\Documents\\Selenium Projects\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else {
+			throw new Exception("Browser is not correct.");
+		}
+	
+	driver.get("http://tumblr.com");
+	
+	LogInFlow.logIn(driver, username, password);
+	
+	}
+	
+	@Test
+	public void newTextPost() {
+		String random = TextPostFlow.generateString(10);
+		
+		String randomTitle = "title" + random;
+		String randomBody = "body" + random;
+		String randomTag = "tag" + random;
+		
+		TextPostFlow.createTextPost(driver, randomTitle, randomBody, randomTag);
+		
+		Assert.assertTrue(TextPostPage.createdTextPost(driver, randomTitle).getText().contains(randomTitle));
+		Assert.assertTrue(TextPostPage.createdTextPost(driver, randomBody).getText().contains(randomBody));
+		Assert.assertTrue(TextPostPage.createdTextPost(driver, randomTag).getText().contains(randomTag));
+		
+	     System.out.println("Successfully created a text post");
+			
+	     // Close the driver
+	     driver.quit();
+		
+	}
+	
+}
